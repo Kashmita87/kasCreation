3a576fdc8160246e8c425d56d55bb5e49399ff0a
Kashmita87 <157013005+Kashmita87@users.noreply.github.com>
2024-01-18 18:20:29 +0530

    Create App.js

---
+++ KAS creation/frontend/kas-creation/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+function App() {
+return (
+  <div classApp="App">
+    <Navbar/>
+  
+    <footer>
+    <Footer/> 
+    </footer>
+    {/* <h1>Hi Ashmita , Welcome to my react page</h1> */}
+  </div>
+  
+  
+ );
+}
+
+export default App;
