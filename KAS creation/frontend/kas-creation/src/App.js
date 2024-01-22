6bb84dddb6bcb2ae9c4439fcdf501f3c167da638
Kashmita87 <157013005+Kashmita87@users.noreply.github.com>
2024-01-22 17:57:48 +0530

    Update App.js

--- KAS creation/frontend/kas-creation/src/App.js
+++ KAS creation/frontend/kas-creation/src/App.js
@@ -7,10 +7,9 @@ return (
   <div classApp="App">
     <Navbar/>
   
-    <footer>
-    <Footer/> 
-    </footer>
-    {/* <h1>Hi Ashmita , Welcome to my react page</h1> */}
+  
+    <Footer/>
+   
   </div>
   
   
