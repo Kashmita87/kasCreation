d48ef81cdf9f57c9ffefc62d836fca3abc704f57
Kashmita87 <157013005+Kashmita87@users.noreply.github.com>
2024-01-24 13:43:25 +0530

    Update App.js

--- KAS creation/frontend/kas-creation/src/App.js
+++ KAS creation/frontend/kas-creation/src/App.js
@@ -1,15 +1,24 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap-icons/font/bootstrap-icons.css";
+import {Route,BrowserRouter,Routes} from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import Home from './components/home';
+import Login from './components/Login';
 function App() {
 return (
   <div classApp="App">
     <Navbar/>
-  
+    {/* <Home/> */}
   
     <Footer/>
-   
+    <BrowserRouter>
+        <Routes>
+            <Route path='/home' Component={Home}/>
+            <Route path='/Login' Component={Login}/>
+        </Routes>
+    </BrowserRouter>
   </div>
   
   
