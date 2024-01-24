9d0d840e851a5498873ba33ffd18e5107e73f1e6
Kashmita87 <157013005+Kashmita87@users.noreply.github.com>
2024-01-24 13:39:35 +0530

    Create home.js

---
+++ KAS creation/frontend/kas-creation/src/components/home.js
@@ -0,0 +1,39 @@
+import Carousel from 'react-bootstrap/Carousel';
+import Image from 'react-bootstrap/Image';
+import imageOne from '../photos/A.jpg';
+import imageTwo from '../photos/b.jpg';
+
+function Home() {
+  return (
+    <Carousel>
+      <Carousel.Item>
+        {/* <ExampleCarouselImage text="First slide" /> */}
+        <Image src={imageOne}/>
+        {/* <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption> */}
+      </Carousel.Item>
+      <Carousel.Item>
+        {/* <ExampleCarouselImage text="Second slide" /> */}
+        <Image src={imageTwo}/>
+        {/* <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption> */}
+      </Carousel.Item>
+      <Carousel.Item>
+        {/* <ExampleCarouselImage text="Third slide" /> */}
+        <Image src={imageOne}/>
+        {/* <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption> */}
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+export default Home;
