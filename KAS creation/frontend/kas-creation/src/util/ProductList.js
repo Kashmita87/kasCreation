af81879a0e2c9f5428ae39e6d3f34861bcd707f8
Kashmita87 <157013005+Kashmita87@users.noreply.github.com>
2024-03-14 18:31:45 +0530

    Create ProductList.js

---
+++ KAS creation/frontend/kas-creation/src/util/ProductList.js
@@ -0,0 +1,125 @@
+const ProductList = [
+    {
+      product_id: 1,
+      name: "Another1",
+      description: "This is another example product description.",
+      price: 29.99,
+      currency: "Rupees",
+      category: "water-art",
+      availability: true,
+      stock_quantity: 50,
+      images:"photos/plant.jpg",
+      ratings: {
+        average_rating: 4.2,
+        total_ratings: 8
+      }
+    },
+    {
+      product_id: 2,
+      name: "Another",
+      description: "This is another example product description.",
+      price: 29.99,
+      currency: "Rupees",
+      category: "water-art",
+      availability: true,
+      stock_quantity: 50,
+      images: "photos/plant.jpg",
+      ratings: {
+        average_rating: 4.2,
+        total_ratings: 80
+      }
+    },
+    {
+      product_id: 3,
+      name: "Another",
+      description: "This is another example product description.",
+      price: 29.99,
+      currency: "Rupees",
+      category: "water-art",
+      availability: true,
+      stock_quantity: 50,
+      images:"photos/plant.jpg",
+      ratings: {
+        average_rating: 4.2,
+        total_ratings: 80
+      }
+    },
+    {
+      product_id: 4,
+      name: "Another",
+      description: "This is another example product description.",
+      price: 29.99,
+      currency: "Rupees",
+      category: "water-art",
+      availability: true,
+      stock_quantity: 50,
+      images: "photos/plant.jpg",
+      ratings: {
+        average_rating: 4.2,
+        total_ratings: 80
+      }
+    },
+    {
+      product_id: 5,
+      name: "Another",
+      description: "This is another example product description.",
+      price: 29.99,
+      currency: "Rupees",
+      category: "water-art",
+      availability: true,
+      stock_quantity: 50,
+      images: "photos/76394.jpg",
+      ratings: {
+        average_rating: 4.2,
+        total_ratings: 80
+      }
+    },
+    {
+      product_id: 6,
+      name: "Another",
+      description: "This is another example product description.",
+      price: 29.99,
+      currency: "Rupees",
+      category: "water-art",
+      availability: true,
+      stock_quantity: 50,
+      images:"photos/76394.jpg",
+      ratings: {
+        average_rating: 4.2,
+        total_ratings: 80
+      }
+    },
+    {
+      product_id: 7,
+      name: "Another",
+      description: "This is another example product description.",
+      price: 29.99,
+      currency: "Rupees",
+      category: "water-art",
+      availability: true,
+      stock_quantity: 50,
+      images: "photos/66384.jpg",
+      ratings: {
+        average_rating: 4.2,
+        total_ratings: 80
+      }
+    },
+    {
+      product_id: 789012,
+      name: "Another",
+      description: "This is another example product description.",
+      price: 29.99,
+      currency: "Rupees",
+      category: "water-art",
+      availability: true,
+      stock_quantity: 50,
+      images: "photos/76394.jpg",
+      ratings: {
+        average_rating: 4.2,
+        total_ratings: 80
+      }
+    }
+  ];
+  
+  export default ProductList;
+  
